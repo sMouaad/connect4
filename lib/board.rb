ea1794314254ab66f8dc65cd9debb2ae4a67d6f9
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-08-31 16:33:39 +0100

    feat(board): add printing board logic

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,53 @@
+# frozen_string_literal: true
+
+# Class with includes logic for board class such as printing logic
 class Board
+  COLORS = %i[yellow red].freeze
+  BOARD_ROWS = 6
+  BOARD_COLUMNS = 7
+  EMPTY_CELL = ' '
+  attr_accessor :board_data
+
+  def initialize
+    @board_data = Array.new(BOARD_ROWS) { Array.new(BOARD_COLUMNS, EMPTY_CELL) }
+  end
+
+  def print_board
+    print_top_line
+    print_rows
+    print_column_numbers
+  end
+
+  private
+
+  def print_rows
+    @board_data.each do |row|
+      print '|'
+      row.each { |element| print " #{element} |" }
+      puts
+      print_bottom_line
+      puts
+    end
+  end
+
+  def print_column_numbers
+    1.upto(BOARD_COLUMNS) do |number|
+      if number < 10
+        print "  #{number} "
+      else
+        print " #{number} "
+      end
+    end
+    puts
+  end
+
+  def print_top_line
+    puts " #{'_' * ((BOARD_COLUMNS * 4) - 1)}"
+  end
+
+  def print_bottom_line
+    print "|#{'___|' * BOARD_COLUMNS}"
+  end
 end
+
+Board.new.print_board
